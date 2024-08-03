0668190523496dc6c29c22244a3094f89a36ef80
João Lucas de Sousa Portella <joaolucassp123@gmail.com>
2024-08-03 01:02:43 -0300

    Remove excel methods

--- Main.cs
+++ Main.cs
@@ -1,66 +1,21 @@
-using ClosedXML.Excel;
-
 namespace CustomerManagement
 {
     public partial class Main : Form
     {
-
         private readonly CustomerForm customerForm;
+        private readonly Database database;
 
         public Main()
         {
             InitializeComponent();
 
             this.customerForm = new CustomerForm();
+            this.database = new Database();
         }
 
         private void MainLoad(object sender, EventArgs e)
         {
-            CreateDataDirectory();
-            CreateDataFile();
-        }
-
-        private void CreateDataDirectory()
-        {
-            try
-            {
-                if (!Directory.Exists(Constants.DataDirectoryPath))
-                {
-                    Directory.CreateDirectory(Constants.DataDirectoryPath);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error creating DATA directory: {ex.Message}");
-            }
-        }
-
-        private void CreateDataFile()
-        {
-            try
-            {
-                if (!File.Exists(Constants.DataFilePath))
-                {
-                    using (var workbook = new XLWorkbook())
-                    {
-                        foreach (var sheet in Constants.worksheetsMapping)
-                        {
-                            var worksheet = workbook.Worksheets.Add(sheet.Key);
-
-                            for (int i = 0; i < sheet.Value.Length; i++)
-                            {
-                                worksheet.Cell(1, i + 1).Value = sheet.Value[i];
-                            }
-                        }
-
-                        workbook.SaveAs(Constants.DataFilePath);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error creating DATA directory: {ex.Message}");
-            }
+            // TODO: fetch sum of total customers and display to user.
         }
 
         private void RegisterCustomerClick(object sender, EventArgs e)
